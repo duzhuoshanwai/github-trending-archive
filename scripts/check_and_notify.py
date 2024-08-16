c26d8a28e9301ae1cd0c50178a6a79fc21136721
duzhuoshanwai <2371431704@qq.com>
2024-08-16 14:27:36 +0800

    test

--- scripts/check_and_notify.py
+++ scripts/check_and_notify.py
@@ -15,13 +15,11 @@ def send_webhook():
     # 构建 GitHub 文件的 URL
     url = f'https://github.com/duzhuoshanwai/github-trending-archive/tree/master/data/{yyyy_mm}/{yyyy_mm_dd}.md'
 
-    # 构建 JSON payload
-    payload = {
-        'url': url
-    }
 
     # 发送 POST 请求到 Webhook
-    response = requests.post(WEBHOOK_URL, json=payload)
+    headers = {"Content-Type": "text/plain"}
+    response = requests.post(WEBHOOK_URL, headers=headers,data=url)
+
     if response.status_code == 200:
         print(f"成功发送通知: {url}")
     else:
